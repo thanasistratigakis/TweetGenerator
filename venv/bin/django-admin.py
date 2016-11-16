4800431f6e42ea7c0e01a375d678a407c5a9d917
Thanasi Stratigakis <thanasi.strati@gmail.com>
2016-11-16 11:20:50 -0800

    ready

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ThanasiStratigakis/Desktop/MakeSchool/Q2/TweetGenerator/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
